b09c1f8599000ef3b09820f4f81828ed1e8dd3b2
James Hillyerd <james@hillyerd.com>
2016-04-03 11:20:45 -0700

    Rename a couple things for clarity
    - Board becomes Game
    - Game.cell becomes Game.board

--- sudoku.go
+++ sudoku.go
@@ -8,65 +8,66 @@ import (
 
 const DIM = 9
 
-// Board represents a sudoku board
-type Board struct {
-	cell       [][]int
+// Game represents a sudoku board
+type Game struct {
+	// board represents the game board, access as board[row][col]
+	board      [][]int
 	remaining  int
 	backtracks int
 }
 
-// NewBoard creates an empty sudoku board
-func NewBoard() *Board {
-	b := &Board{}
-	// Build empty (zero) cell matrix
-	b.cell = make([][]int, DIM)
-	for i := range b.cell {
-		b.cell[i] = make([]int, DIM)
+// NewGame creates an empty sudoku board
+func NewGame() *Game {
+	g := &Game{}
+	// Build empty (zero) board matrix
+	g.board = make([][]int, DIM)
+	for i := range g.board {
+		g.board[i] = make([]int, DIM)
 	}
-	b.remaining = DIM * DIM
-	return b
+	g.remaining = DIM * DIM
+	return g
 }
 
 // String formats the board for human consumption
-func (b *Board) String() string {
+func (g *Game) String() string {
 	var result = "    1 2 3 4 5 6 7 8 9\n"
-	for i, row := range b.cell {
+	for i, row := range g.board {
 		result += fmt.Sprintf("%v: %v\n", i+1, row)
 	}
-	result += fmt.Sprintf("Remaining: %v, Backtracks: %v", b.remaining, b.backtracks)
+	result += fmt.Sprintf("Remaining: %v, Backtracks: %v", g.remaining, g.backtracks)
 	return result
 }
 
 // ValidSolution is true if remaining == 0
-func (b *Board) ValidSolution() bool {
-	return b.remaining == 0
+func (g *Game) ValidSolution() bool {
+	return g.remaining == 0
 }
 
 // MakeMove adds a number to the board, row and col indices are 0 based
-func (b *Board) MakeMove(row, col, val int) {
-	if b.cell[row][col] == 0 && val != 0 {
-		b.remaining--
+func (g *Game) MakeMove(row, col, val int) {
+	if g.board[row][col] == 0 && val != 0 {
+		g.remaining--
 	}
-	b.cell[row][col] = val
+	g.board[row][col] = val
 }
 
 // UnmakeMove removes a number from the board, row and col indices are 0 based
-func (b *Board) UnmakeMove(row, col int) {
-	if b.cell[row][col] != 0 {
-		b.remaining++
-		b.cell[row][col] = 0
+func (g *Game) UnmakeMove(row, col int) {
+	if g.board[row][col] != 0 {
+		g.remaining++
+		g.board[row][col] = 0
 	}
-	b.backtracks++
+	g.backtracks++
 }
 
 // NextEmptyCell tells our solver which cell to work on next
-func (b *Board) NextEmptyCell() (row, col int) {
+func (g *Game) NextEmptyCell() (row, col int) {
 	min := DIM + 1
-	for ri, cols := range b.cell {
+	for ri, cols := range g.board {
 		for ci, val := range cols {
 			if val == 0 {
 				cur := 0
-				candidates := b.CellCandidates(ri, ci)
+				candidates := g.CellCandidates(ri, ci)
 				// Count candidates
 				for i := 1; i <= DIM; i++ {
 					if candidates[i] {
@@ -85,7 +86,7 @@ func (b *Board) NextEmptyCell() (row, col int) {
 }
 
 // CellCandidates returns a list of legal moves for specified cell
-func (b *Board) CellCandidates(row, col int) []bool {
+func (g *Game) CellCandidates(row, col int) []bool {
 	if row < 0 || DIM < row {
 		panic(fmt.Sprintf("Invalid row passed: %v", row))
 	}
@@ -100,11 +101,11 @@ func (b *Board) CellCandidates(row, col int) []bool {
 	}
 	// Check row
 	for i := 0; i < DIM; i++ {
-		candidates[b.cell[row][i]] = false
+		candidates[g.board[row][i]] = false
 	}
 	// Check column
 	for i := 0; i < DIM; i++ {
-		candidates[b.cell[i][col]] = false
+		candidates[g.board[i][col]] = false
 	}
 	// Check section
 	rowStart := row / 3 * 3
@@ -113,7 +114,7 @@ func (b *Board) CellCandidates(row, col int) []bool {
 	colEnd := colStart + DIM/3
 	for ri := rowStart; ri < rowEnd; ri++ {
 		for ci := colStart; ci < colEnd; ci++ {
-			candidates[b.cell[ri][ci]] = false
+			candidates[g.board[ri][ci]] = false
 		}
 	}
 	return candidates
@@ -121,38 +122,38 @@ func (b *Board) CellCandidates(row, col int) []bool {
 
 // recursiveSolver tries to solve the board using a recursive backtracking
 // algorithm
-func recursiveSolver(b *Board) (solved bool) {
-	if b.ValidSolution() {
+func recursiveSolver(g *Game) (solved bool) {
+	if g.ValidSolution() {
 		return true
 	}
 
-	row, col := b.NextEmptyCell()
-	candidates := b.CellCandidates(row, col)
+	row, col := g.NextEmptyCell()
+	candidates := g.CellCandidates(row, col)
 
 	// Try each candidate
 	for val, avail := range candidates {
 		if avail {
-			b.MakeMove(row, col, val)
-			solved = recursiveSolver(b)
+			g.MakeMove(row, col, val)
+			solved = recursiveSolver(g)
 			if solved {
 				break
 			}
 			// Move was incorrect
-			b.UnmakeMove(row, col)
+			g.UnmakeMove(row, col)
 		}
 	}
 
 	return solved
 }
 
-// readBoard reads a board from a text file, ignoring non-numeric characters
-func readBoard(fname string) (*Board, error) {
+// readGame reads a board from a text file, ignoring non-numeric characters
+func readGame(fname string) (*Game, error) {
 	file, err := os.Open(fname)
 	if err != nil {
 		return nil, err
 	}
 	scanner := bufio.NewScanner(file)
-	b := NewBoard()
+	b := NewGame()
 	for row := 0; row < DIM; row++ {
 		if !scanner.Scan() {
 			return nil, fmt.Errorf("EOF while reading row %v", row+1)
@@ -179,11 +180,11 @@ func readBoard(fname string) (*Board, error) {
 
 // validateSolution cross checks each cell of the board.  Not part of the
 // solver, but used to validate the solvers correctness.
-func validateSolution(b Board) {
+func validateSolution(b Game) {
 	for row := 0; row < DIM; row++ {
 		for col := 0; col < DIM; col++ {
-			expect := b.cell[row][col]
-			b.cell[row][col] = 0
+			expect := b.board[row][col]
+			b.board[row][col] = 0
 			candidates := b.CellCandidates(row, col)
 			if !candidates[expect] {
 				fmt.Printf("Invalid value %v at row %v, col %v\n", expect, row+1, col+1)
@@ -197,7 +198,7 @@ func main() {
 		fmt.Println("Puzzle filename required")
 		os.Exit(1)
 	}
-	board, err := readBoard(os.Args[1])
+	board, err := readGame(os.Args[1])
 	if err != nil {
 		fmt.Println(err)
 		return
